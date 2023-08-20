1e93b8d14c9d642062d6c10191074b55d706cada
santatra <razafindratsimba34@gmail.com>
2023-08-20 12:31:44 +0300

    rename old models.py to persistance.py'

---
+++ render/persistance.py
@@ -0,0 +1,24 @@
+# All the models here are custom models, since we are using a flat-file database
+import os
+import json
+
+parent_dir = os.path.join(os.path.dirname(__file__), os.pardir)
+dataPath = os.path.join(parent_dir, 'data.json')
+data = {}
+
+with open(dataPath, 'r') as file:
+	data = json.load(file)
+
+def get_all():
+	all = []
+	for element in data.values():
+		all.append(element)
+	return all
+
+def get_room(room_name):
+	all = []
+	for element in data.values():
+		if element['room'] == room_name:
+			all.append(element)
+	return all
+
