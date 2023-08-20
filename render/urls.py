41a370f70631961149ab5015dc56013aefb75bef
santatra <razafindratsimba34@gmail.com>
2023-08-20 11:11:59 +0300

    add the templates (urls, views, html)

--- render/urls.py
+++ render/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
         path('', views.index, name='index'),
+        path('logout/', views.index, name='logout'),
+        path("room/<str:room_name>/", views.room, name="room"),
         ]
--- render/views.py
+++ render/views.py
@@ -1,4 +1,17 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request, 'render/index.html', {})
+    rooms = [{'name':'lobby'}, {'name':'test'}]
+    context = {
+        'username': '_santatra',
+        'rooms': rooms,
+        'room_name': '',
+    }
+    return render(request, 'render/index.html', context)
+
+def room(request, room_name):
+    context = {
+    'username': '_santatra',
+    "room_name": room_name
+    }
+    return render(request, 'render/room.html', context)
